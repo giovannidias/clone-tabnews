ab543e0d72a136f9f2a356c1e03a6d20fd67827f
Giovanni Dias Marinheiro de Oliveira <giovannidias.cp@gmail.com>
2025-12-09 02:59:40 +0000

    add `lint:check` and `lint:fix` scripts

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto de estudo, implementa clone do https://tabnews.com.br
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1> Te amo mô! Fica estressada não! x3 </h1>
+  return <h1> Te amo mô! Fica estressada não! x3 </h1>;
 }
 
-export default Home
+export default Home;
